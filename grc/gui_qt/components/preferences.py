b839bc0227a56244d51145af215a0f5d33924dac
Håkon Vågsether <hakon.vagsether@gmail.com>
2024-02-25 16:06:30 +0100

    grcqt: Add runtime settings to preferences
    Signed-off-by: Håkon Vågsether <hakon.vagsether@gmail.com>

--- grc/gui_qt/components/preferences.py
+++ grc/gui_qt/components/preferences.py
@@ -4,13 +4,14 @@ from __future__ import absolute_import, print_function
 import logging
 import yaml
 
-from qtpy.QtCore import Qt
+from qtpy.QtCore import Qt, QSettings
 from qtpy.QtWidgets import (QLineEdit, QTabWidget, QDialog,
                             QScrollArea, QVBoxLayout, QCheckBox,
                             QComboBox, QHBoxLayout, QDialogButtonBox,
                             QLabel, QWidget, QFormLayout)
 
 from ..properties import Paths
+from gnuradio import gr
 
 # Logging
 log = logging.getLogger(f"grc.application.{__name__}")
@@ -28,6 +29,8 @@ class PreferencesDialog(QDialog):
 
         self.setWindowTitle("GRC Preferences")
         self.tab_widget = QTabWidget()
+
+        self.rt_prefs = QSettings(gr.userconf_path() + "/config.conf", QSettings.IniFormat)
 
         with open(Paths.AVAILABLE_PREFS_YML) as available_prefs_yml:
             self.pref_dict = yaml.safe_load(available_prefs_yml)
@@ -99,6 +102,46 @@ class PreferencesDialog(QDialog):
 
                 tab.form.addRow(item['_label'], item['_edit'])
 
+        rt_tab = self.new_tab_widget()
+        for rt_cat in self.pref_dict['runtime']:
+            for item in rt_cat['items']:
+                full_key = rt_cat['key'] + '/' + item['key']
+
+                item['_label'] = QLabel(item['name'])
+
+                if item['dtype'] == 'bool':
+                    item['_edit'] = QCheckBox()
+
+                    if self.rt_prefs.contains(full_key):
+                        value = self.rt_prefs.value(full_key, False, type=bool)
+                        item['_edit'].setChecked(value)
+                    else:
+                        item['_edit'].setChecked(item['default'])
+                        self.rt_prefs.setValue(full_key, item['default'])
+
+                elif item['dtype'] == 'enum':
+                    item['_edit'] = QComboBox()
+                    for opt in item['option_labels']:
+                        item['_edit'].addItem(opt)
+                    index = item['options'].index(self.rt_prefs.value(full_key, item['default'], type=str))
+                    item['_edit'].setCurrentIndex(index)
+                else:
+                    if self.rt_prefs.contains(full_key):
+                        item['_edit'] = QLineEdit(self.rt_prefs.value(full_key))
+                    else:
+                        item['_edit'] = QLineEdit(str(item['default']))
+                        self.rt_prefs.setValue(full_key, item['default'])
+
+                item['_line'] = QHBoxLayout()
+
+                if 'tooltip' in item.keys():
+                    item['_label'].setToolTip(item['tooltip'])
+                    item['_edit'].setToolTip(item['tooltip'])
+
+                rt_tab.form.addRow(item['_label'], item['_edit'])
+
+        self.tab_widget.addTab(rt_tab, 'Runtime')
+
     def save_all(self):
         log.debug(f'Writing changes to {self.qsettings.fileName()}')
 
@@ -114,3 +157,16 @@ class PreferencesDialog(QDialog):
                     self.qsettings.setValue(full_key, item['_edit'].text())
 
         self.qsettings.sync()
+
+        for rt_cat in self.pref_dict['runtime']:
+            for item in rt_cat['items']:
+                full_key = rt_cat['key'] + '/' + item['key']
+
+                if item['dtype'] == 'bool':
+                    self.rt_prefs.setValue(full_key, item['_edit'].isChecked())
+                elif item['dtype'] == 'enum':
+                    self.rt_prefs.setValue(full_key, item['options'][item['_edit'].currentIndex()])
+                else:
+                    self.rt_prefs.setValue(full_key, item['_edit'].text())
+
+        self.rt_prefs.sync()
